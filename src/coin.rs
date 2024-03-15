6cd60c234e15b93190941f6ec3f21da3b4b83311
Onur Ereren <onur.ereren998@gmail.com>
2024-03-15 21:04:15 +0100

    Added collectable coins

---
+++ src/coin.rs
@@ -0,0 +1,58 @@
+use std::time::Duration;
+
+use bevy::{
+    diagnostic::{DiagnosticsStore, FrameTimeDiagnosticsPlugin},
+    prelude::*,
+    sprite::{MaterialMesh2dBundle, Mesh2dHandle},
+};
+use rand::{distributions::Uniform, prelude::*};
+
+use crate::game_state::GameState;
+
+pub struct CoinPlugin;
+
+impl Plugin for CoinPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(CoinTimer(Timer::from_seconds(1.0, TimerMode::Repeating)))
+            .add_systems(Update, spawn_coin);
+    }
+}
+
+#[derive(Resource)]
+struct CoinTimer(Timer);
+
+#[derive(Component)]
+pub struct Coin;
+
+fn spawn_coin(
+    mut commands: Commands,
+    mut windows: Query<&mut Window>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut coin_timer: ResMut<CoinTimer>,
+    time: Res<Time>,
+    state: Res<State<GameState>>,
+) {
+    if state.get() != &GameState::InGame {
+        return;
+    }
+    if coin_timer.0.tick(time.delta()).finished() {
+        let window = windows.single_mut();
+        let window_size = Vec2::new(window.width() as f32, window.height() as f32);
+        let offset = 30.0;
+
+        let mut rng = thread_rng();
+
+        let x = rng.gen_range((-window_size.x / 2.0) + offset..(window_size.x / 2.0) - offset);
+        let y = rng.gen_range((-window_size.y / 2.0) + offset..(window_size.y / 2.0) - offset);
+
+        commands
+            .spawn(MaterialMesh2dBundle {
+                mesh: Mesh2dHandle(meshes.add(Circle::new(5.0))),
+                material: materials.add(Color::RED),
+                transform: Transform::from_xyz(x, y, 0.0),
+                ..default()
+            })
+            .insert(Coin);
+    }
+}
