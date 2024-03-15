d3b0ecb7dc9494a93d82e4af67a6f1e62c4ea4cb
Onur Ereren <onur.ereren998@gmail.com>
2024-03-15 21:03:58 +0100

    Added fps label

---
+++ src/fps.rs
@@ -0,0 +1,55 @@
+use bevy::{
+    diagnostic::{DiagnosticsStore, FrameTimeDiagnosticsPlugin},
+    prelude::*,
+};
+
+use crate::game_state::ScoreText;
+
+pub struct FpsPlugin;
+
+impl Plugin for FpsPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugins(FrameTimeDiagnosticsPlugin)
+            .add_systems(Startup, setup)
+            .add_systems(Update, text_update_system);
+    }
+}
+
+#[derive(Component)]
+struct FpsText;
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn((
+        TextBundle::from_sections([
+            TextSection::new(
+                "FPS: ",
+                TextStyle {
+                    // This font is loaded and will be used instead of the default font.
+                    font: asset_server.load("font/FiraSans-Bold.ttf"),
+                    font_size: 20.0,
+                    color: Color::WHITE,
+                },
+            ),
+            TextSection::from_style(TextStyle {
+                font_size: 20.0,
+                color: Color::WHITE,
+                ..default()
+            }),
+        ]),
+        FpsText,
+    ));
+}
+
+fn text_update_system(
+    diagnostics: Res<DiagnosticsStore>,
+    mut query: Query<&mut Text, With<FpsText>>,
+) {
+    for mut text in &mut query {
+        if let Some(fps) = diagnostics.get(&FrameTimeDiagnosticsPlugin::FPS) {
+            if let Some(value) = fps.smoothed() {
+                // Update the value of the second section
+                text.sections[1].value = format!("{value:.2}");
+            }
+        }
+    }
+}
